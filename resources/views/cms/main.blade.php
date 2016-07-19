d025976e81d7ed1e86b2607fd76ad81b9f80b222
Eric Disseldorp <eric@goedemiddag.nl>
2016-07-19 14:14:54 +0200

    main blade

--- resources/views/cms/main.blade.php
+++ resources/views/cms/main.blade.php
@@ -8,6 +8,31 @@
 
 <div class="row">
     <div class="col-md-12">
-        {!! CMS::tinymce('content') !!}
+        {!! CMS::group('blok1', ['titleblok1', 'Titel', 'text']) !!}
+    </div>
+    <div class="col-md-12">
+        {!! CMS::tinymce('content1') !!}
+    </div>
+</div>
+
+<hr />
+
+<div class="row">
+    <div class="col-md-12">
+        {!! CMS::group('blok2', ['titleblok2', 'Titel', 'text']) !!}
+    </div>
+    <div class="col-md-12">
+        {!! CMS::tinymce('content2') !!}
+    </div>
+</div>
+
+<hr />
+
+<div class="row">
+    <div class="col-md-12">
+        {!! CMS::group('blok3', ['titleblok3', 'Titel', 'text']) !!}
+    </div>
+    <div class="col-md-12">
+        {!! CMS::tinymce('content3') !!}
     </div>
 </div>

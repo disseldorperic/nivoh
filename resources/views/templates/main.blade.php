7c707e6aabf645887ef1f323d5b06bfc8753f9dd
Eric Disseldorp <eric@goedemiddag.nl>
2016-07-19 14:34:03 +0200

    image

--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -3,7 +3,49 @@
 @section('content')
     @include('partials._title')
 
-    <section class="white" id="mainTemp">
+        <section class="white textTemp">
+            @if (Content::get('blok1.image') != '')
+                <div class="image">
+                    <img src="{!! Content::image('blok1.image') ?: '/images/placeholder.jpg' !!}" class="img-responsive" alt="{!! Content::get('blok1.title') !!}" />
+                </div>
+            @endif
 
-    </section>
+            <h1>
+                {!! Content::get('blok1.title') !!}
+            </h1>
+
+            {!! Content::get('content1') !!}
+        </section>
+
+    @if (Content::get('content2') != '')
+        <section class="white textTemp">
+            @if (Content::get('blok2.image') != '')
+                <div class="image">
+                    <img src="{!! Content::image('blok1.image') ?: '/images/placeholder.jpg' !!}" class="img-responsive" alt="{!! Content::get('blok1.title') !!}" />
+                </div>
+            @endif
+
+            <h1>
+                {!! Content::get('blok2.title') !!}
+            </h1>
+
+            {!! Content::get('content2') !!}
+        </section>
+    @endif
+
+    @if (Content::get('content3') != '')
+        <section class="white textTemp">
+            @if (Content::get('blok3.image') != '')
+                <div class="image">
+                    <img src="{!! Content::image('blok1.image') ?: '/images/placeholder.jpg' !!}" class="img-responsive" alt="{!! Content::get('blok1.title') !!}" />
+                </div>
+            @endif
+
+            <h1>
+                {!! Content::get('blok3.title') !!}
+            </h1>
+
+            {!! Content::get('content3') !!}
+        </section>
+    @endif
 @endsection

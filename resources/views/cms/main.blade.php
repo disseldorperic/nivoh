b3d1dda722d921bbe7472f195d435d1dd4cdfbff
Eric Disseldorp <eric@goedemiddag.nl>
2016-07-19 14:08:56 +0200

    main blade

--- resources/views/cms/main.blade.php
+++ resources/views/cms/main.blade.php
@@ -4,6 +4,7 @@
     </div>
 </div>
 
+<!--
 <hr />
 
 <div class="row">
@@ -11,15 +12,7 @@
         {!! CMS::input('short', 'Content', 'textarea', ['min-length' => 50, 'max-length' => 150, 'required']) !!}
     </div>
 </div>
-
-<div class="row">
-    <div class="col-md-12">
-        {!! CMS::upload('main', 'Foto', ['type' => 'image', 'width' => 600, 'height' => 100]) !!}
-    </div>
-    <div class="col-md-12">
-        {!! CMS::group('images', ['image1', 'Image 1', 'image', ['width' => 200, 'height' => 200, 'cropper' => true]], ['image2', 'Image 2', 'image', ['width' => 400, 'height' => 200]]) !!}
-    </div>
-</div>
+-->
 
 <hr />
 

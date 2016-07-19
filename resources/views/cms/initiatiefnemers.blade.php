5a8b628c709463e480b16d02739ff0bbd7bc3169
Eric Disseldorp <eric@goedemiddag.nl>
2016-07-19 16:08:55 +0200

    linkgpush logo

--- resources/views/cms/initiatiefnemers.blade.php
+++ resources/views/cms/initiatiefnemers.blade.php
@@ -1,5 +1,10 @@
 <div class="row">
     <div class="col-md-12">
-        {!! CMS::group('initiatiefnemer', ['name', 'Naam', 'text'], ['link', 'Link', 'text'], ['image', 'Afbeelding', 'image', ['width' => 400, 'height' => 400, 'cropper' => false]]) !!}
+        {!! CMS::group('initiatiefnemer', ['name', 'Naam', 'text'], ['link', 'Link', 'text']) !!}
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-12">
+        {!! CMS::upload('image', 'Logo', ['type' => 'image', 'width' => 667, 'height' => null, 'cropper' => false]) !!}
     </div>
 </div>
--- resources/views/partials/_initiatiefnemers.blade.php
+++ resources/views/partials/_initiatiefnemers.blade.php
@@ -4,7 +4,7 @@
             <div class="col-md-4">
                 <div class="initiatiefnemer">
                     <a id="box-1" class="box" href="{{ $item->get('initiatiefnemer.link') }}" target="_blank">
-                        <img id="image-1" src="{!! $item->image('initiatiefnemer.image') ?: '/images/nologo.jpg' !!}" alt="{{ $item->get('initiatiefnemer.name') }} />
+                        <img id="image-1" src="{!! $item->image('image') ?: '/images/nologo.jpg' !!}" alt="{{ $item->get('initiatiefnemer.name') }} />
                     <span class="caption">
                     <p>{{ $item->get('initiatiefnemer.name') }}</p>
                     </span>

2d07b220fc8d21b5b979b07df837897f1fcfab25
Eric Disseldorp <eric@goedemiddag.nl>
2016-09-06 12:45:56 +0200

    nivoh faq

--- resources/views/cms/faq.blade.php
+++ resources/views/cms/faq.blade.php
@@ -7,7 +7,9 @@
 <hr />
 
 <div class="row">
-
+    <div class="col-md-12">
+        {!! CMS::group('blok1', ['title', 'Titel', 'text'], ['image', 'Afbeelding', 'image', ['width' => 800, 'height' => 200, 'cropper' => true]]) !!}
+    </div>
 
     <div class="col-md-12">
         {!! CMS::tinymce('content') !!}

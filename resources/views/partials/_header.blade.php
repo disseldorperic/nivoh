a18f9f648e794349b1233572c50e1bcb90eb4acd
Eric Disseldorp <eric@goedemiddag.nl>
2016-09-16 09:27:40 +0200

    logo retina

--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <a class="logo" href="/"><img src="/images/logo.png" class="img-responsive" alt="Stichting Nivoh" /></a>
+                <a class="logo" href="/"><img src="/images/logo@2x.png" width="329" height="87" class="img-responsive" alt="Stichting Nivoh" /></a>
             </div>
         </div>
     </div>

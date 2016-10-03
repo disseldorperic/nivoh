6a05f238276224c2186bfb7d26c27e2c78d38792
Eric Disseldorp <eric@goedemiddag.nl>
2016-10-03 11:15:35 +0200

    correctie IE keuze menu

--- resources/views/templates/home.blade.php
+++ resources/views/templates/home.blade.php
@@ -10,32 +10,38 @@
             Wilt u <span>meer informatie</span>? Maak hieronder uw keuze.
         </h1>
 
-        <a class="button" id="btnInstallateur" href="{!! Content::link('installateur') !!}">
-            <span class="ikben">Ik ben een:</span>
+        <div class="row">
+            <div class="col-sm-6">
+                <a class="button" id="btnInstallateur" href="{!! Content::link('installateur') !!}">
+                    <span class="ikben">Ik ben een:</span>
 
-            <div class="branche">
-                <span class="name">Adviseur</span>
-                <span class="name">Installateur</span>
-                <span class="name">Inspecteur</span>
+                    <div class="branche">
+                        <span class="name">Adviseur</span>
+                        <span class="name">Installateur</span>
+                        <span class="name">Inspecteur</span>
+                    </div>
+                    <div class="click">
+                        Klik hier <i class="fa fa-long-arrow-right"></i>
+                    </div>
+                </a>
             </div>
-            <div class="click">
-                Klik hier <i class="fa fa-long-arrow-right"></i>
-            </div>
-        </a>
+            <div class="col-sm-6">
+                <a class="button" id="btnOpdrachtgever" href="{!! Content::link('opdrachtgever') !!}">
+                    <span class="ikben">Ik ben een:</span>
 
-        <a class="button" id="btnOpdrachtgever" href="{!! Content::link('opdrachtgever') !!}">
-            <span class="ikben">Ik ben een:</span>
+                    <div class="branche">
+                        <span class="name">Opdrachtgever</span>
+                        <span class="name">Branchevereniging</span>
+                        <span class="name">Architect</span>
+                        <span class="name">Anders</span>
+                    </div>
+                    <div class="click">
+                        Klik hier <i class="fa fa-long-arrow-right"></i>
+                    </div>
+                </a>
+            </div>
+        </div>
 
-            <div class="branche">
-                <span class="name">Opdrachtgever</span>
-                <span class="name">Branchevereniging</span>
-                <span class="name">Architect</span>
-                <span class="name">Anders</span>
-            </div>
-            <div class="click">
-                Klik hier <i class="fa fa-long-arrow-right"></i>
-            </div>
-        </a>
     </section>
 
     <section class="white" id="news">

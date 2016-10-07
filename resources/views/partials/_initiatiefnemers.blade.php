a8b3a0140223cb4ba4a744fc126237e98ec4b8ad
Eric Disseldorp <eric@goedemiddag.nl>
2016-10-07 08:27:13 +0200

    images initiatiefnemers

--- resources/views/partials/_initiatiefnemers.blade.php
+++ resources/views/partials/_initiatiefnemers.blade.php
@@ -4,7 +4,7 @@
             <div class="col-md-4">
                 <div class="initiatiefnemer">
                     <a class="box" href="{{ $item->get('initiatiefnemer.link') }}" target="_blank">
-                        <img src="{!! $item->image('image') ?: '/images/nologo.jpg' !!}" alt="{{ $item->get('initiatiefnemer.name') }}" />
+                        <img src="{!! $item->image('image') ?: '/images/nologo.jpg' !!}" class="img-responsive" alt="{{ $item->get('initiatiefnemer.name') }}" />
                         <span>naar website <i class="fa fa-chevron-right"></i></span>
                     </a>
                 </div>

90b15b5082d9f3ad32a3a2f3ae2b9b01558536ea
Eric Disseldorp <eric@goedemiddag.nl>
2016-10-14 08:44:41 +0200

    initiatiefnemers logo

--- resources/views/partials/_initiatiefnemers.blade.php
+++ resources/views/partials/_initiatiefnemers.blade.php
@@ -1,10 +1,10 @@
 <section class="initiatiefnemers">
     <div class="row">
         @foreach (Content::items('initiatiefnemers') AS $item)
-            <div class="col-md-4">
+            <div class="col-md-4 col-sm-6 col-xs-6">
                 <div class="initiatiefnemer">
                     <a class="box" href="{{ $item->get('initiatiefnemer.link') }}" target="_blank">
-                        <img src="{!! $item->image('image') ?: '/images/nologo.jpg' !!}" class="img-responsive" alt="{{ $item->get('initiatiefnemer.name') }}" />
+                        <img src="{!! $item->image('image') ?: '/images/nologo.jpg' !!}" class="img-responsive" width="230" height="180" alt="{{ $item->get('initiatiefnemer.name') }}" />
                         <span>naar website <i class="fa fa-chevron-right"></i></span>
                     </a>
                 </div>
